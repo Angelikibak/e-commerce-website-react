d455ff7b173009fc3a9cbe8f888e91aa3f334f05
Angeliki Bakogianni <agelikibakogianni@gmail.com>
2021-07-19 17:14:57 +0200

    CSS tweaks

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './menu-item.scss';
 
-const subTitle = 'Shop Now';
+const subTitle = 'SHOP NOW';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-   <div
-      className={`${size ? 'large menu-item' : 'menu-item'}`}
-      style={{ backgroundImage: `url(${imageUrl})` }}
-   >
+   <div className={`${size ? 'large menu-item' : 'menu-item'}`}>
+      <div
+         className='background-image'
+         style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className='content'>
-         <h1 className='title'>{title}</h1>
+         <h1 className='title'>{title.toUpperCase()}</h1>
          <span className='subtitle'>{subTitle}</span>
       </div>
    </div>
